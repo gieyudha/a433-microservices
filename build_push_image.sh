66f636cc3a6d148ea0e47154ac9f65041aaa16e4
Fergie <fergieyudha@gmail.com>
2023-09-12 15:49:25 +0700

    update

--- build_push_image.sh
+++ build_push_image.sh
@@ -6,11 +6,11 @@ docker build -t item-app:v1 .
 # Melihat daftar images pada docker
 docker images
 
-# Mengubah nama image agar sesuai dengan format Docker Hub
+# Mengubah nama image agar sesuai dengan format github package
 docker tag item-app:v1 ghcr.io/gieyudha/item-app:v1
 
-# Login ke github packages
+# Login ke github package
 echo $PAT | docker login ghcr.io --username gieyudha --password-stdin
 
-# Push image ke docker hub
+# Push image ke github package
 docker push ghcr.io/gieyudha/item-app:v1
